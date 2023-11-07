6b1419e3985abda7802e8310bae63159b4c2a166
Eliezer Graber <me@eygraber.com>
2023-11-06 23:17:37 -0500

    First version of the action

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,89 +1,652 @@
-/**
- * Unit tests for the action's main functionality, src/main.ts
- *
- * These should be run as if the action was called from a workflow.
- * Specifically, the inputs listed in `action.yml` should be set as environment
- * variables following the pattern `INPUT_<INPUT_NAME>`.
- */
-
 import * as core from '@actions/core'
-import * as main from '../src/main'
+import * as github from '@actions/github'
+import { run } from '../src/main'
 
-// Mock the action's main function
-const runMock = jest.spyOn(main, 'run')
+jest.mock('@actions/core', () => ({
+  getInput: jest.fn().mockImplementation((name: string) => {
+    switch (name) {
+      case 'github-token':
+        return 'test-token'
+      case 'label':
+        return 'autobase'
+      case 'required-approvals':
+        return '0'
+      case 'base-branch':
+        return ''
+      default:
+        return ''
+    }
+  }),
+  setFailed: jest.fn(),
+  info: jest.fn(),
+  error: jest.fn()
+}))
 
-// Other utilities
-const timeRegex = /^\d{2}:\d{2}:\d{2}/
+const mockListPulls = jest.fn()
+const mockGetPull = jest.fn()
+const mockUpdateBranch = jest.fn()
+const mockListReviews = jest.fn()
+const mockRepoGet = jest.fn().mockImplementation(() => {
+  return {
+    data: {
+      default_branch: 'master'
+    }
+  }
+})
 
-// Mock the GitHub Actions core library
-let debugMock: jest.SpyInstance
-let errorMock: jest.SpyInstance
-let getInputMock: jest.SpyInstance
-let setFailedMock: jest.SpyInstance
-let setOutputMock: jest.SpyInstance
+const mockOctokit = {
+  rest: {
+    pulls: {
+      list: mockListPulls,
+      get: mockGetPull,
+      updateBranch: mockUpdateBranch,
+      listReviews: mockListReviews
+    },
 
-describe('action', () => {
+    repos: {
+      get: mockRepoGet
+    }
+  }
+}
+
+jest.mock('@octokit/rest', () => {
+  return {
+    Octokit: jest.fn().mockImplementation(() => {
+      return mockOctokit
+    })
+  }
+})
+
+jest.mock('@actions/github', () => ({
+  context: {
+    repo: {
+      owner: 'owner',
+      repo: 'repo'
+    },
+    payload: {
+      action: 'completed',
+      pull_request: {
+        number: 1,
+        merged: true,
+        base: {
+          ref: 'master'
+        },
+        head: {
+          sha: 'abc123'
+        }
+      },
+      check_suite: {
+        conclusion: 'failure',
+        pull_requests: [
+          {
+            number: 1
+          }
+        ]
+      },
+      getOctokit: jest.fn().mockImplementation(() => {
+        return mockOctokit
+      })
+    },
+    eventName: 'pull_request'
+  }
+}))
+
+describe('Auto Rebase Action', () => {
   beforeEach(() => {
     jest.clearAllMocks()
-
-    debugMock = jest.spyOn(core, 'debug').mockImplementation()
-    errorMock = jest.spyOn(core, 'error').mockImplementation()
-    getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
-    setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
-    setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()
   })
 
-  it('sets the time output', async () => {
-    // Set the action's inputs as return values from core.getInput()
-    getInputMock.mockImplementation((name: string): string => {
-      switch (name) {
-        case 'milliseconds':
-          return '500'
-        default:
-          return ''
+  it('Use default input values', async () => {
+    if (github.context.payload.pull_request) {
+      github.context.payload.pull_request.base.ref = 'main'
+    }
+
+    jest
+      .spyOn(core, 'getInput')
+      .mockImplementationOnce(() => 'test-token')
+      .mockImplementationOnce(() => '')
+      .mockImplementationOnce(() => '')
+      .mockImplementationOnce(() => '')
+
+    mockRepoGet.mockResolvedValueOnce({
+      data: {
+        default_branch: 'main'
       }
     })
 
-    await main.run()
-    expect(runMock).toHaveReturned()
+    mockListPulls.mockResolvedValueOnce({
+      data: [
+        {
+          number: 2,
+          labels: [{ name: 'autobase' }],
+          draft: false,
+          mergeable_state: 'behind',
+          head: { sha: 'def456' }
+        }
+      ]
+    })
 
-    // Verify that all of the core library functions were called correctly
-    expect(debugMock).toHaveBeenNthCalledWith(1, 'Waiting 500 milliseconds ...')
-    expect(debugMock).toHaveBeenNthCalledWith(
-      2,
-      expect.stringMatching(timeRegex)
-    )
-    expect(debugMock).toHaveBeenNthCalledWith(
-      3,
-      expect.stringMatching(timeRegex)
-    )
-    expect(setOutputMock).toHaveBeenNthCalledWith(
-      1,
-      'time',
-      expect.stringMatching(timeRegex)
-    )
-    expect(errorMock).not.toHaveBeenCalled()
-  })
-
-  it('sets a failed status', async () => {
-    // Set the action's inputs as return values from core.getInput()
-    getInputMock.mockImplementation((name: string): string => {
-      switch (name) {
-        case 'milliseconds':
-          return 'this is not a number'
-        default:
-          return ''
+    mockGetPull.mockResolvedValueOnce({
+      data: {
+        rebaseable: true,
+        mergeable_state: 'behind'
       }
     })
 
-    await main.run()
-    expect(runMock).toHaveReturned()
+    mockUpdateBranch.mockResolvedValueOnce({
+      data: {
+        url: 'https://api.github.com/repos/owner/repo/pulls/2'
+      }
+    })
 
-    // Verify that all of the core library functions were called correctly
-    expect(setFailedMock).toHaveBeenNthCalledWith(
-      1,
-      'milliseconds not a number'
+    await run()
+
+    expect(mockListReviews).not.toHaveBeenCalled()
+    expect(mockListPulls).toHaveBeenCalledTimes(1)
+    expect(mockGetPull).toHaveBeenCalledTimes(1)
+    expect(mockUpdateBranch).toHaveBeenCalledWith({
+      owner: 'owner',
+      repo: 'repo',
+      pull_number: 2,
+      expected_head_sha: 'def456',
+      update_method: 'rebase'
+    })
+
+    if (github.context.payload.pull_request) {
+      github.context.payload.pull_request.base.ref = 'master'
+    }
+  })
+
+  it('Does not rebase when the event is not supported', async () => {
+    github.context.eventName = 'some-event'
+
+    await run()
+
+    expect(mockListPulls).not.toHaveBeenCalled()
+    expect(mockUpdateBranch).not.toHaveBeenCalled()
+    expect(core.setFailed).toHaveBeenCalledWith(
+      'This action only supports pull_request and check_suite events.'
     )
-    expect(errorMock).not.toHaveBeenCalled()
+
+    github.context.eventName = 'pull_request'
+  })
+
+  it('Rebases the next PR correctly when a PR is merged into the default branch', async () => {
+    mockListPulls.mockResolvedValueOnce({
+      data: [
+        {
+          number: 2,
+          labels: [{ name: 'autobase' }],
+          draft: false,
+          mergeable_state: 'behind',
+          head: { sha: 'def456' }
+        }
+      ]
+    })
+
+    mockGetPull.mockResolvedValueOnce({
+      data: {
+        rebaseable: true,
+        mergeable_state: 'behind'
+      }
+    })
+
+    mockUpdateBranch.mockResolvedValueOnce({
+      data: {
+        url: 'https://api.github.com/repos/owner/repo/pulls/2'
+      }
+    })
+
+    await run()
+
+    expect(mockListPulls).toHaveBeenCalledTimes(1)
+    expect(mockGetPull).toHaveBeenCalledTimes(1)
+    expect(mockUpdateBranch).toHaveBeenCalledWith({
+      owner: 'owner',
+      repo: 'repo',
+      pull_number: 2,
+      expected_head_sha: 'def456',
+      update_method: 'rebase'
+    })
+  })
+
+  it('Rebases the next PR correctly when a check suite does not succeed for a PR with the specified label', async () => {
+    github.context.eventName = 'check_suite'
+
+    mockGetPull.mockResolvedValueOnce({
+      data: {
+        labels: [{ name: 'autobase' }]
+      }
+    })
+
+    mockListPulls.mockResolvedValueOnce({
+      data: [
+        {
+          number: 2,
+          labels: [{ name: 'autobase' }],
+          draft: false,
+          mergeable_state: 'behind',
+          head: { sha: 'def456' }
+        }
+      ]
+    })
+
+    mockGetPull.mockResolvedValueOnce({
+      data: {
+        rebaseable: true,
+        mergeable_state: 'behind'
+      }
+    })
+
+    mockUpdateBranch.mockResolvedValueOnce({
+      data: {
+        url: 'https://api.github.com/repos/owner/repo/pulls/2'
+      }
+    })
+
+    await run()
+
+    expect(mockListPulls).toHaveBeenCalledTimes(1)
+    expect(mockGetPull).toHaveBeenCalledTimes(2)
+    expect(mockUpdateBranch).toHaveBeenCalledWith({
+      owner: 'owner',
+      repo: 'repo',
+      pull_number: 2,
+      expected_head_sha: 'def456',
+      update_method: 'rebase'
+    })
+
+    github.context.eventName = 'pull_request'
+  })
+
+  it('Does not rebase when the pull_request payload is not present', async () => {
+    const payload = github.context.payload
+    github.context.payload = {}
+
+    await run()
+
+    expect(mockListPulls).not.toHaveBeenCalled()
+    expect(mockUpdateBranch).not.toHaveBeenCalled()
+    expect(core.setFailed).toHaveBeenCalledWith(
+      'Event payload missing `pull_request`'
+    )
+
+    github.context.payload = payload
+  })
+
+  it('Does not rebase when the check_suite payload is not present', async () => {
+    github.context.eventName = 'check_suite'
+    const payload = github.context.payload
+    github.context.payload = {}
+
+    await run()
+
+    expect(mockListPulls).not.toHaveBeenCalled()
+    expect(mockUpdateBranch).not.toHaveBeenCalled()
+    expect(core.setFailed).toHaveBeenCalledWith(
+      'Event payload missing `check_suite`'
+    )
+
+    github.context.eventName = 'pull_request'
+    github.context.payload = payload
+  })
+
+  it('Does not rebase when a check suite action is not completed', async () => {
+    github.context.eventName = 'check_suite'
+    github.context.payload.action = 'requested'
+
+    await run()
+
+    expect(mockListPulls).not.toHaveBeenCalled()
+    expect(mockUpdateBranch).not.toHaveBeenCalled()
+    expect(core.info).toHaveBeenCalledWith(
+      'Ignoring a check_suite event with a requested action and failure conclusion'
+    )
+
+    github.context.payload.action = 'completed'
+    github.context.eventName = 'pull_request'
+  })
+
+  it('Does not rebase when a check suite is a success', async () => {
+    github.context.eventName = 'check_suite'
+    github.context.payload.check_suite.conclusion = 'success'
+
+    await run()
+
+    expect(mockListPulls).not.toHaveBeenCalled()
+    expect(mockUpdateBranch).not.toHaveBeenCalled()
+    expect(core.info).toHaveBeenCalledWith(
+      'Ignoring a check_suite event with a completed action and success conclusion'
+    )
+
+    github.context.payload.check_suite.conclusion = 'failure'
+    github.context.eventName = 'pull_request'
+  })
+
+  it('Does not rebase when a check suite does not have any PRs', async () => {
+    const prs = github.context.payload.check_suite.pull_requests
+    github.context.eventName = 'check_suite'
+    github.context.payload.check_suite.pull_requests = []
+
+    await run()
+
+    expect(mockListPulls).not.toHaveBeenCalled()
+    expect(mockUpdateBranch).not.toHaveBeenCalled()
+    expect(core.info).toHaveBeenCalledWith(
+      'No pull requests are associated with the check suite.'
+    )
+
+    github.context.payload.check_suite.pull_requests = prs
+    github.context.eventName = 'pull_request'
+  })
+
+  it('Does not rebase when a check suite does not have any PRs with the specified label', async () => {
+    github.context.eventName = 'check_suite'
+
+    mockGetPull.mockResolvedValueOnce({
+      data: {
+        labels: [{ name: 'some-other-label' }]
+      }
+    })
+
+    await run()
+
+    expect(mockGetPull).toHaveBeenCalled()
+    expect(mockListPulls).not.toHaveBeenCalled()
+    expect(mockUpdateBranch).not.toHaveBeenCalled()
+    expect(core.info).toHaveBeenCalledWith(
+      "Skipping check suite for PR #1 since it does not have the label 'autobase'."
+    )
+
+    github.context.eventName = 'pull_request'
+  })
+
+  it('Does not rebase if the PR merged is not into the default branch', async () => {
+    // Change the base ref to simulate a PR merged into a non-default branch
+    if (github.context.payload.pull_request) {
+      github.context.payload.pull_request.base.ref = 'develop'
+    }
+
+    await run()
+
+    expect(mockListPulls).not.toHaveBeenCalled()
+    expect(mockUpdateBranch).not.toHaveBeenCalled()
+
+    if (github.context.payload.pull_request) {
+      github.context.payload.pull_request.base.ref = 'master'
+    }
+  })
+
+  it('Does not rebase if the PR does not have the specified label', async () => {
+    mockListPulls.mockResolvedValueOnce({
+      data: [
+        {
+          number: 3,
+          labels: [{ name: 'some-other-label' }],
+          draft: false,
+          mergeable_state: 'behind',
+          head: { sha: 'ghi789' }
+        }
+      ]
+    })
+
+    await run()
+
+    expect(mockListPulls).toHaveBeenCalledTimes(1)
+    expect(mockUpdateBranch).not.toHaveBeenCalled()
+  })
+
+  it('Does not rebase if the PR is a draft', async () => {
+    mockListPulls.mockResolvedValueOnce({
+      data: [
+        {
+          number: 4,
+          labels: [{ name: 'autobase' }],
+          draft: true,
+          mergeable_state: 'behind',
+          head: { sha: 'jkl012' }
+        }
+      ]
+    })
+
+    await run()
+
+    expect(mockListPulls).toHaveBeenCalledTimes(1)
+    expect(mockUpdateBranch).not.toHaveBeenCalled()
+  })
+
+  it('Does not rebase if the PR is not behind the default branch', async () => {
+    mockListPulls.mockResolvedValueOnce({
+      data: [
+        {
+          number: 5,
+          labels: [{ name: 'autobase' }],
+          draft: false,
+          mergeable_state: 'clean',
+          head: { sha: 'mno345' }
+        }
+      ]
+    })
+
+    await run()
+
+    expect(mockListPulls).toHaveBeenCalledTimes(1)
+    expect(mockUpdateBranch).not.toHaveBeenCalled()
+  })
+
+  it('Does not rebase if the PR does not have the required number of approvals', async () => {
+    jest
+      .spyOn(core, 'getInput')
+      .mockImplementationOnce(() => 'test-token')
+      .mockImplementationOnce(() => 'autobase')
+      .mockImplementationOnce(() => '2')
+
+    mockGetPull.mockResolvedValueOnce({
+      data: {
+        rebaseable: true,
+        mergeable_state: 'behind'
+      }
+    })
+
+    mockListPulls.mockResolvedValueOnce({
+      data: [
+        {
+          number: 6,
+          labels: [{ name: 'autobase' }],
+          draft: false,
+          mergeable_state: 'behind',
+          head: { sha: 'pqr678' }
+        }
+      ]
+    })
+
+    mockListReviews.mockResolvedValueOnce({
+      data: [{ state: 'APPROVED' }]
+    })
+
+    await run()
+
+    expect(mockListPulls).toHaveBeenCalledTimes(1)
+    expect(mockListReviews).toHaveBeenCalledTimes(1)
+    expect(mockUpdateBranch).not.toHaveBeenCalled()
+  })
+
+  it('Handles errors during the rebase process', async () => {
+    mockListPulls.mockResolvedValueOnce({
+      data: [
+        {
+          number: 7,
+          labels: [{ name: 'autobase' }],
+          draft: false,
+          mergeable_state: 'behind',
+          head: { sha: 'stu901' }
+        }
+      ]
+    })
+
+    mockGetPull.mockResolvedValueOnce({
+      data: {
+        rebaseable: true,
+        mergeable_state: 'behind'
+      }
+    })
+
+    mockUpdateBranch.mockRejectedValueOnce(new Error('Rebase failed'))
+
+    await run()
+
+    expect(mockListPulls).toHaveBeenCalledTimes(1)
+    expect(mockGetPull).toHaveBeenCalledTimes(1)
+    expect(mockUpdateBranch).toHaveBeenCalledWith({
+      owner: 'owner',
+      repo: 'repo',
+      pull_number: 7,
+      expected_head_sha: 'stu901',
+      update_method: 'rebase'
+    })
+    expect(core.setFailed).toHaveBeenCalledWith(
+      'Failed to rebase PR #7: Error: Rebase failed'
+    )
+  })
+
+  it('Rebases only PRs targeting a specific base branch if configured', async () => {
+    mockListPulls.mockResolvedValueOnce({
+      data: [
+        {
+          number: 8,
+          labels: [{ name: 'autobase' }],
+          draft: false,
+          base: { ref: 'master' },
+          mergeable_state: 'behind',
+          head: { sha: 'abc123' }
+        },
+        {
+          number: 9,
+          labels: [{ name: 'autobase' }],
+          draft: false,
+          base: { ref: 'develop' },
+          mergeable_state: 'behind',
+          head: { sha: 'def456' }
+        }
+      ]
+    })
+
+    mockGetPull.mockResolvedValueOnce({
+      data: {
+        rebaseable: true,
+        mergeable_state: 'behind'
+      }
+    })
+
+    await run()
+
+    expect(mockUpdateBranch).toHaveBeenCalledWith(
+      expect.objectContaining({ pull_number: 8 })
+    )
+    expect(mockUpdateBranch).toHaveBeenCalledTimes(1)
+  })
+
+  it('Handles the scenario where there are no pull requests to process', async () => {
+    mockListPulls.mockResolvedValueOnce({ data: [] })
+
+    await run()
+
+    expect(mockUpdateBranch).not.toHaveBeenCalled()
+    expect(core.setFailed).not.toHaveBeenCalled()
+  })
+
+  it('Skips PRs that are already up-to-date with the base branch', async () => {
+    mockListPulls.mockResolvedValueOnce({
+      data: [
+        {
+          number: 10,
+          labels: [{ name: 'autobase' }],
+          draft: false,
+          mergeable_state: 'clean',
+          head: { sha: 'ghi789' }
+        }
+      ]
+    })
+
+    mockGetPull.mockResolvedValueOnce({
+      data: {
+        rebaseable: true,
+        mergeable_state: 'clean'
+      }
+    })
+
+    await run()
+
+    expect(mockUpdateBranch).not.toHaveBeenCalled()
+  })
+
+  it('Skips PRs with a merge conflict', async () => {
+    mockListPulls.mockResolvedValueOnce({
+      data: [
+        {
+          number: 11,
+          labels: [{ name: 'autobase' }],
+          draft: false,
+          mergeable_state: 'dirty',
+          head: { sha: 'jkl012' }
+        }
+      ]
+    })
+
+    mockGetPull.mockResolvedValueOnce({
+      data: {
+        rebaseable: true,
+        mergeable_state: 'dirty'
+      }
+    })
+
+    await run()
+
+    expect(mockUpdateBranch).not.toHaveBeenCalled()
+  })
+
+  it('Skips PRs that are closed', async () => {
+    mockListPulls.mockResolvedValueOnce({
+      data: [
+        {
+          number: 12,
+          labels: [{ name: 'autobase' }],
+          draft: false,
+          state: 'closed',
+          mergeable_state: 'behind',
+          head: { sha: 'mno345' }
+        }
+      ]
+    })
+
+    await run()
+
+    expect(mockUpdateBranch).not.toHaveBeenCalled()
+  })
+
+  it('Skips PRs that are not rebaseable', async () => {
+    mockListPulls.mockResolvedValueOnce({
+      data: [
+        {
+          number: 12,
+          labels: [{ name: 'autobase' }],
+          draft: false,
+          state: 'open',
+          mergeable_state: 'behind',
+          head: { sha: 'mno345' }
+        }
+      ]
+    })
+
+    mockGetPull.mockResolvedValueOnce({
+      data: {
+        rebaseable: false,
+        mergeable_state: 'behind'
+      }
+    })
+
+    await run()
+
+    expect(mockUpdateBranch).not.toHaveBeenCalled()
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,192 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as github from '@actions/github'
+import { Octokit } from '@octokit/rest'
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const token = core.getInput('github-token', { required: true })
+    const label = core.getInput('label', { required: false }) || 'autobase'
+    const requiredApprovals: number = parseInt(
+      core.getInput('required-approvals', { required: false }) || '0',
+      10
+    )
+    const octokit = new Octokit({ auth: token })
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const { context } = github
+    const { owner, repo } = context.repo
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const repoInfo = await octokit.rest.repos.get({
+      owner,
+      repo
+    })
+    const defaultBranch = repoInfo.data.default_branch
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const baseBranch =
+      core.getInput('base-branch', { required: false }) || defaultBranch
+
+    if (context.eventName === 'pull_request') {
+      const pullRequest = context.payload.pull_request
+      if (!pullRequest) {
+        core.setFailed('Event payload missing `pull_request`')
+        return
+      }
+
+      const wasMerged = pullRequest.merged
+      const baseRef = pullRequest.base.ref
+
+      if (wasMerged && baseRef === baseBranch) {
+        await rebaseNextPullRequest(
+          octokit,
+          owner,
+          repo,
+          label,
+          requiredApprovals,
+          baseBranch
+        )
+      }
+    } else if (context.eventName === 'check_suite') {
+      const checkSuite = context.payload.check_suite
+      if (!checkSuite) {
+        core.setFailed('Event payload missing `check_suite`')
+        return
+      }
+
+      const { action, check_suite } = context.payload
+
+      if (action === 'completed' && check_suite.conclusion !== 'success') {
+        const { pull_requests } = check_suite
+
+        if (pull_requests.length === 0) {
+          core.info('No pull requests are associated with the check suite.')
+          return
+        }
+
+        for (const pr of pull_requests) {
+          // Get the full PR object to access its labels
+          const { data: fullPr } = await octokit.rest.pulls.get({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            pull_number: pr.number
+          })
+
+          // Check if the PR has the specified label
+          const hasLabel = fullPr.labels.some(
+            fullPrLabel => fullPrLabel.name === core.getInput('label')
+          )
+
+          if (!hasLabel) {
+            core.info(
+              `Skipping check suite for PR #${
+                pr.number
+              } since it does not have the label '${core.getInput('label')}'.`
+            )
+            continue
+          }
+
+          await rebaseNextPullRequest(
+            octokit,
+            owner,
+            repo,
+            label,
+            requiredApprovals,
+            baseBranch
+          )
+        }
+      } else {
+        core.info(
+          `Ignoring a check_suite event with a ${action} action and ${checkSuite.conclusion} conclusion`
+        )
+      }
+    } else {
+      core.setFailed(
+        `This action only supports pull_request and check_suite events.`
+      )
+    }
   } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+    core.setFailed(`Action failed with error: ${error}`)
   }
 }
+
+async function rebaseNextPullRequest(
+  octokit: Octokit,
+  owner: string,
+  repo: string,
+  label: string,
+  requiredApprovals: number,
+  baseBranch: string
+): Promise<void> {
+  const { data: pullRequests } = await octokit.rest.pulls.list({
+    owner,
+    repo,
+    state: 'open',
+    base: baseBranch,
+    sort: 'created',
+    direction: 'asc'
+  })
+
+  for (const pr of pullRequests) {
+    if (!pr.labels.map(prLabel => prLabel.name).includes(label)) {
+      core.info(`PR #${pr.number} is not labeled with '${label}'.`)
+      continue
+    }
+
+    if (pr.draft) {
+      core.info(`PR #${pr.number} is a draft PR.`)
+      continue
+    }
+
+    const { data: prData } = await octokit.rest.pulls.get({
+      owner,
+      repo,
+      pull_number: pr.number
+    })
+
+    if (prData.mergeable_state !== 'behind') {
+      core.info(
+        `PR #${pr.number} is not 'behind' (was '${prData.mergeable_state}').`
+      )
+      continue
+    }
+
+    if (!prData.rebaseable) {
+      core.info(`PR #${pr.number} is not rebaseable.`)
+      continue
+    }
+
+    if (requiredApprovals > 0) {
+      const { data: reviews } = await octokit.rest.pulls.listReviews({
+        owner,
+        repo,
+        pull_number: pr.number
+      })
+
+      const approvalCount = reviews.filter(
+        review => review.state === 'APPROVED'
+      ).length
+      if (approvalCount < requiredApprovals) {
+        core.info(
+          `PR #${pr.number} requires ${requiredApprovals} approvals, but only has${approvalCount}.`
+        )
+        continue
+      }
+    }
+
+    try {
+      const { data: rebaseResult } = await octokit.rest.pulls.updateBranch({
+        owner,
+        repo,
+        pull_number: pr.number,
+        expected_head_sha: pr.head.sha,
+        update_method: 'rebase'
+      })
+
+      core.info(`Rebased PR #${pr.number}: ${rebaseResult.url}`)
+      return
+    } catch (error) {
+      core.setFailed(`Failed to rebase PR #${pr.number}: ${error}`)
+      continue
+    }
+  }
+}
+
+run()
